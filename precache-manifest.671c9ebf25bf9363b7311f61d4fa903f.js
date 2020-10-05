2bbe71ec9305e7e9f90005489c224df178af5e7c
sehwan505 <sehwan505@gmail.com>
2020-10-05 18:19:03 +0900

    Updates

---
+++ precache-manifest.671c9ebf25bf9363b7311f61d4fa903f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c337e907e6bbea46dbcd50b65e5099ea",
+    "url": "/movie-app-2020/index.html"
+  },
+  {
+    "revision": "49716e9211d0d2e3f3a3",
+    "url": "/movie-app-2020/static/css/main.29d2e494.chunk.css"
+  },
+  {
+    "revision": "9a78590711f880ba06ca",
+    "url": "/movie-app-2020/static/js/2.1ccb8562.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app-2020/static/js/2.1ccb8562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49716e9211d0d2e3f3a3",
+    "url": "/movie-app-2020/static/js/main.ee82e1f9.chunk.js"
+  },
+  {
+    "revision": "b03465f25580502d820c",
+    "url": "/movie-app-2020/static/js/runtime-main.3e7f16a7.js"
+  }
+]);
